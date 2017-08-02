c7c95cd86dac96afc3393459b26fb2f59c54d861
NaRan <nrn.panthi@gmail.com>
2017-08-02 21:35:46 +0545

    Update search.php

--- search.php
+++ search.php
@@ -34,7 +34,7 @@ if (isset($_GET['q']) && isset($_GET['maxResults'])) {
    * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
    * Please ensure that you have enabled the YouTube Data API for your project.
    */
-  $DEVELOPER_KEY = 'AIzaSyDC4xq4kdyLnU-hXAkerQU7z8Ff3KlhvHQ';
+  $DEVELOPER_KEY = 'YOUR_API';
 
   $client = new Google_Client();
   $client->setDeveloperKey($DEVELOPER_KEY);
